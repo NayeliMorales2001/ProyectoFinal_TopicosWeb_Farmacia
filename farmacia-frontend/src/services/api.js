e754256a1f15f9667ac82c3a4c6f9017919e0adf
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-05-31 17:53:31 -0600

    correccion url

--- farmacia-frontend/src/services/api.js
+++ farmacia-frontend/src/services/api.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
-    timeout: 120000, // 2 minutos
+    baseURL: "https://proyectofinal-topicosweb-farmacia-1.onrender.com/api",
+    timeout: 120000,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
     },
 });
 
-// TOKEN
 api.interceptors.request.use((config) => {
-
     const token = localStorage.getItem("token");
 
     if (token) {
@@ -21,15 +19,11 @@ api.interceptors.request.use((config) => {
     return config;
 });
 
-// ERRORES
 api.interceptors.response.use(
-
     (response) => response,
 
     (error) => {
-
         console.error("API ERROR:", error);
-
         return Promise.reject(error);
     }
 );
