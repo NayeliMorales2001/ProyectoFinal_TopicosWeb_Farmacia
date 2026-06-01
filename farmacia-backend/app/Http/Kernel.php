7b6a97e6e95569dc1ac981970155b4aa26cb9031
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-06-01 09:43:49 -0600

    correccion de rutas

--- farmacia-backend/app/Http/Kernel.php
+++ farmacia-backend/app/Http/Kernel.php
@@ -64,42 +64,51 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
 
-        'auth' =>
-            \App\Http\Middleware\Authenticate::class,
+    'auth' =>
+        \App\Http\Middleware\Authenticate::class,
 
-        'auth.basic' =>
-            \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+    'auth.basic' =>
+        \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
 
-        'auth.session' =>
-            \Illuminate\Session\Middleware\AuthenticateSession::class,
+    'auth.session' =>
+        \Illuminate\Session\Middleware\AuthenticateSession::class,
 
-        'cache.headers' =>
-            \Illuminate\Http\Middleware\SetCacheHeaders::class,
+    'cache.headers' =>
+        \Illuminate\Http\Middleware\SetCacheHeaders::class,
 
-        'can' =>
-            \Illuminate\Auth\Middleware\Authorize::class,
+    'can' =>
+        \Illuminate\Auth\Middleware\Authorize::class,
 
-        'guest' =>
-            \App\Http\Middleware\RedirectIfAuthenticated::class,
+    'guest' =>
+        \App\Http\Middleware\RedirectIfAuthenticated::class,
 
-        'password.confirm' =>
-            \Illuminate\Auth\Middleware\RequirePassword::class,
+    'password.confirm' =>
+        \Illuminate\Auth\Middleware\RequirePassword::class,
 
-        'precognitive' =>
-            \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
+    'precognitive' =>
+        \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
 
-        'signed' =>
-            \App\Http\Middleware\ValidateSignature::class,
+    'signed' =>
+        \App\Http\Middleware\ValidateSignature::class,
 
-        'throttle' =>
-            \Illuminate\Routing\Middleware\ThrottleRequests::class,
+    'throttle' =>
+        \Illuminate\Routing\Middleware\ThrottleRequests::class,
 
-        'verified' =>
-            \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    'verified' =>
+        \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
-        // CUSTOM
-        'admin' =>
-            \App\Http\Middleware\AdminMiddleware::class,
+    // SPATIE
+    'role' =>
+        \Spatie\Permission\Middleware\RoleMiddleware::class,
 
-    ];
+    'permission' =>
+        \Spatie\Permission\Middleware\PermissionMiddleware::class,
+
+    'role_or_permission' =>
+        \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+
+    // CUSTOM
+    'admin' =>
+        \App\Http\Middleware\AdminMiddleware::class,
+];
 }
